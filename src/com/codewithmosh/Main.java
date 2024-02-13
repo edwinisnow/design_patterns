21b2ecd4f44bfb566178d91462d11b0ed101a425
EDWIN GEORGE LAZER <edwin04@gmail.com>
2024-02-13 04:30:09 +0000

    first commit

---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,12 @@
+package com.codewithmosh;
+
+public class Main {
+
+	public static void main(String[] args) {
+		User user = new User();
+		user.name = "Mosh";
+		System.out.println(user.name);
+
+	}
+
+}
---
+++ src/com/codewithmosh/User.java
@@ -0,0 +1,14 @@
+package com.codewithmosh;
+
+public class User {
+
+	public String name;
+	
+	public User(String name) {
+		this.name = name;
+		
+	}
+	
+	
+
+}
